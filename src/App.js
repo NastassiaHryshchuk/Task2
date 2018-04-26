7950322c9c2d9702f1236d7605188194a950d889
Nastassia Hryshchuk <Nastassia_Hryshchuk@epam.com>
2018-04-26 18:50:21 +0300

    added Films and Footer components with styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
+import Films from './Films/Films';
+import Footer from './Footer/Footer';
 
 import './App.css';
 import styles from './_scss/main.scss';
@@ -7,7 +9,11 @@ import styles from './_scss/main.scss';
 class App extends Component {
   render() {
     return (
+      <div className="xxx">
         <Header />
+        <Films />
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+        <footer className="footer">
+            <div className="wrap">
+                <p className="logo">netflixroulette</p>
+            </div>
+         </footer>
+    );
+  }
+}
+
+export default Footer;
