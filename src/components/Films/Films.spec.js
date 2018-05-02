f3a1b514c11876aeba1e150f722955f547516259
Nastassia Hryshchuk <Nastassia_Hryshchuk@epam.com>
2018-05-02 16:58:20 +0300

    added enzyme test with bugs

--- src/components/Films/Films.spec.js
+++ src/components/Films/Films.spec.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
 import Films from './Films';
 import Film from './Film/Film';
 
-configure({ adapter: new Adapter() });
-
 describe('<Films />', () => {
-  it('should render one <Film /> items', () => {
-    const wrapper = shallow(<Films />);
-    expect(wrapper.find(Film).toHaveLength(1));
+  it('should render one <Film /> item', () => {
+    const wrapper = shallow(<Films films={[
+      {
+        id: 'xxx',
+        image: 'xxx',
+        title: 'xxx',
+        releasedate: 111,
+        genre: 'xxx1',
+      }]} />);
+    expect(wrapper.find(Film)).to.have.length(1);
   });
 });
