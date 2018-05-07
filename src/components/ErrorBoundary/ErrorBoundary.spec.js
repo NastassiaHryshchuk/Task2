9e02510ef6a2dc02fc29344c7b033c3dccadee3b
Nastassia Hryshchuk <Nastassia_Hryshchuk@epam.com>
2018-05-07 20:15:21 +0300

    correct work of ErrorBoundary test

--- src/components/ErrorBoundary/ErrorBoundary.spec.js
+++ src/components/ErrorBoundary/ErrorBoundary.spec.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ErrorBoundary from './ErrorBoundary';
 
-class ProblemChild  extends Component {
-  render() {
-    throw new Error('I crashed!');
-  }
-}
+const ProblemChild = () => {
+  throw new Error('I crashed!');
+  return <p>Error</p>;
+};
 
 describe('<ErrorBoundary />', () => {
   it('should catch errors with componentDidCatch', () => {
     const spy = sinon.spy(ErrorBoundary.prototype, 'componentDidCatch');
     const wrapper = mount(<ErrorBoundary><ProblemChild /></ErrorBoundary>);
-    expect(ErrorBoundary.prototype.componentDidCatch).to.have.been.called();
+    expect(ErrorBoundary.prototype.componentDidCatch.calledOnce).to.equal(true);
   });
 });
