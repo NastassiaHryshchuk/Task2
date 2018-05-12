d0eb130d201ccac2e8b4f87797485792cf57c0bc
Nastassia Hryshchuk <19anastasia86@gmail.com>
2018-05-12 21:14:16 +0300

    added redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './containers/App/App';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app'),
 );
 
